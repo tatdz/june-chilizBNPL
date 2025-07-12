ddcdace61adaf11bbbafeb7ed02a3356326c966b
Tatiana Dzhambinova <tatianadzhambinova@Tatianas-MacBook-Air.local>
2025-07-12 15:00:53 +0200

    updates

--- server/index.ts
+++ server/index.ts
@@ -56,13 +56,13 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
+  // ALWAYS serve the app on port 8080
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = process.env.PORT || 8080;
   server.listen({
     port,
-    host: "0.0.0.0",
+    host: "127.0.0.1",
     reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
